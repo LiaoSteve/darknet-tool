cfaa431b19a09db71565491f0ff248ea516406ab
steve-Liao <steveliao1688@gmail.com>
2020-07-15 19:41:48 +0800

    fix generate.py

--- generate_train.py
+++ generate_train.py
@@ -1,12 +1,12 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("data", "obj"))
+os.chdir("./VOCdevkit/VOC2007/JPEGImages")
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("data/obj/" + filename)
-os.chdir("..")
-with open("train.txt", "w") as outfile:
+        image_files.append("data/VOCdevkit/VOC2007/JPEGImages/" + filename)
+os.chdir("../../../")
+with open("2007_train.txt", "w") as outfile:
     for image in image_files:
         outfile.write(image)
         outfile.write("\n")
