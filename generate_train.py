5a8348f4ecec430c2059dc2d4e8288098d369784
ti-2080 <steveliao1688@gmail.com>
2020-07-15 20:44:11 +0800

    revise generate.py

--- generate_train.py
+++ generate_train.py
@@ -1,10 +1,11 @@
 import os
 
 image_files = []
+path = os.getcwd()
 os.chdir("./VOCdevkit/VOC2007/JPEGImages")
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("data/VOCdevkit/VOC2007/JPEGImages/" + filename)
+        image_files.append(path + "/VOCdevkit/VOC2007/JPEGImages/" + filename)
 os.chdir("../../../")
 with open("2007_train.txt", "w") as outfile:
     for image in image_files:
