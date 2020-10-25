2bc21f84e8c15f3fea851121a91f130aeaf29b82
LiaoSteve <steveliao1688@gmail.com>
2020-10-25 18:08:11 +0800

    .....

--- generate_train.py
+++ generate_train.py
@@ -4,7 +4,8 @@ image_files = []
 path = os.getcwd()
 os.chdir("./VOCdevkit/VOC2007/JPEGImages")
 for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".jpg"):
+    if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".jpeg") \
+        or filename.endswith(".JPEG") or filename.endswith(".png") or filename.endswith(".PNG"): 
         image_files.append(path + "/VOCdevkit/VOC2007/JPEGImages/" + filename)
 os.chdir("../../../")
 with open("2007_train.txt", "w") as outfile:
@@ -13,3 +14,4 @@ with open("2007_train.txt", "w") as outfile:
         outfile.write("\n")
     outfile.close()
 os.chdir("..")
+
