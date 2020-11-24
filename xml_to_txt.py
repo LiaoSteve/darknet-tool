31358902c4658ce5a156a16ab8737f74805c12f4
LiaoSteve <steveliao1688@gmail.com>
2020-11-24 12:24:50 +0800

    analysis dataset

--- xml_to_txt.py
+++ xml_to_txt.py
@@ -13,9 +13,11 @@ sets=[('2007', 'dont_care')]
 
 classes = ['garbage','bottle']
 
-global num 
-global P_object
-global N_sample
+num = 0
+P_object = [0 for _ in classes]
+N_sample = 0
+difficult_object = 0
+not_my_desired_object = 0
 
 def convert(size, box):
     dw = 1./size[0]
@@ -31,47 +33,56 @@ def convert(size, box):
     return (x,y,w,h)
 
 def convert_annotation(year, image_id):
-    
+    global N_sample
+    global not_my_desired_object
+    global difficult_object
+    global P_object
+    global num 
     try:
         in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    except Exception as e:
-        global N_sample
+    except Exception as e:        
         N_sample += 1
         print(e)
         return
+
     out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
     tree=ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
+    
+    if root.find('object') is None:        
+        N_sample += 1        
 
-    for obj in root.iter('object'):
+    for obj in root.iter('object'):        
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult) == 1:            
+
+        if cls not in classes or int(difficult) == 1:     
+            print(f'Skip {cls} object, difficult {int(difficult)}')       
+            if cls not in classes:                
+                not_my_desired_object += 1
+            if int(difficult) == 1:                
+                difficult_object += 1
             continue
-        cls_id = classes.index(cls)
-        global P_object
+
+        cls_id = classes.index(cls)        
         P_object[cls_id] += 1
         xmlbox = obj.find('bndbox')
-        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
-        
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))        
         bb = convert((w,h), b)
+
         if bb[0]>1 or bb[1]>1 or bb[2]>1 or bb[3]>1 :            
             raise RuntimeError(f'{image_id} bbox out of range >1')
 
         if bb[0]<0 or bb[1]<0 or bb[2]<0 or bb[3]<0 :            
             raise RuntimeError(f'{image_id} bbox out of range <0')
-        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
-    global num 
+
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')    
     num += 1
 
 wd = getcwd()
-
-num = 0
-P_object = [0 for _ in classes]
-N_sample = 0
 
 for year, image_set in sets:
     if not os.path.exists('VOCdevkit/VOC%s/labels/'%(year)):        
@@ -108,9 +119,11 @@ print(f'- [x] Turn {len(xml_ids)} .xml files to {num} .txt files')
 print(f'- [x] JPEGImages: {len(image_ids)} images')
 n_label = os.listdir('VOCdevkit/VOC2007/labels/')
 print(f'- [x] label: {len(n_label)}')
-print(f'---- info ----')
+print(f'---- Info ----')
 for i in range(len(classes)):
     print(f'- [x] {classes[i]}: {P_object[i]} objects')
-print(f'- [x] Negtive image: {N_sample}')
+print(f'- [x] Negative image: {N_sample}')
+print(f'- [x] difficult object: {difficult_object}')
+print(f'- [x] not my desired object: {not_my_desired_object}')
 
 
