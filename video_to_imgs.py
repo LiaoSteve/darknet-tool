9c200dc3bea42131902e5d87a38b04b65750ec47
LiaoSteve <steveliao1688@gmail.com>
2021-04-23 11:42:36 +0800

    minor fix

--- video_to_imgs.py
+++ video_to_imgs.py
@@ -5,24 +5,26 @@
 import os 
 import cv2
 
-input_path = 'trash7.MP4'
-out_dir    = 'images'
+input_path = 'FPV.avi'
+out_dir    = 'images_FPV'
 
 os.makedirs(out_dir, exist_ok=True)
 cap = cv2.VideoCapture(input_path)
 fps = int(cap.get(5))
-
+all_frame = int(cap.get(7))
 count = 0
 num = 0
-out_fps = 1
+desired_fps = 2 # get image per second
+out_fps = int(fps/desired_fps)
 
 while True:
     ret, img = cap.read()
     count += 1    
     if ret:
-        if count % fps < out_fps:
+        if count % out_fps == 0:
             num += 1
-            cv2.imwrite(out_dir+'/out_'+str(num)+'.jpg', img)
-            #print(num)
+            cv2.imwrite(out_dir+'/'+out_dir+'_'+str(num)+'.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            #print(f"{count}/{all_frame}")
     else: break
 print(f'- [x] Done: save {num} images')
+
